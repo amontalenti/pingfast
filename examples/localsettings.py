c09c7817fd5c11b94d72ecc259e62a5898c42251
Andrew Montalenti <andrew@parsely.com>
2012-04-10 08:51:33 -0400

    example localsettings file

---
+++ examples/localsettings.py
@@ -0,0 +1,14 @@
+thresholds = {
+    '1234': 1000,  # Check with ID 1234 has threshold of 1000ms (1s)
+    '4567': 400    # Check with ID 4567 has threshold of 400ms (0.4s)
+}
+
+#primary pingdom account
+primary_username = "your_pingdom_username"
+primary_password = "*********************"
+primary_appkey = "12345XXXXXXXXXXXXXXX"
+
+#secondary account
+secondary_username = "your_pingdom_username"
+secondary_password = "*********************"
+secondary_appkey = "12345XXXXXXXXXXXXX"
